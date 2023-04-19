f75300327805ae18fef00546b3148c2ffa0a1e61
AndrewCerveny <andrewfcerveny0628@gmail.com>
2023-04-18 23:08:58 -0400

    Add a DetailedArticles page with content flowing in the correct spot

--- src/components/DetailedArticle/DetailedArticle.js
+++ src/components/DetailedArticle/DetailedArticle.js
@@ -1,11 +1,35 @@
 
 import React from "react";
+import { Link } from "react-router-dom";
+import './DetailedArticle.css'
 
 const DetailedArticle = ({matchedArticle}) => {
-  
-  return(<section>
-    
-      <h1> This should be a new page </h1>
+  if(matchedArticle){
+  return(
+  <section className="bleak-page">
+    <section className="title-display">
+      <h2>{matchedArticle.title}</h2>
+    </section>
+    <section className="author-spot">
+      <h3>{matchedArticle.byline}</h3> 
+      <h3> Published: {matchedArticle.publishedDate}</h3>
+      <h3> Updated:{matchedArticle.updatedDate}</h3>     
+    </section>
+    <section className="article-syn">
+      <img src={matchedArticle.multimedia[1].url} alt={matchedArticle.multimedia[1].caption}/>
+      <section className="abstract">
+       <p>{matchedArticle.abstract}</p>
+      </section>
+    </section>
+    <div className="button-wrap">
+      <a href={matchedArticle.shortUrl} target="_blank" rel="noopener noreferrer">
+        <button className="art-btn"> Read More</button>
+      </a>
+      <Link to='/'>
+        <button className="art-btn"> See Articles</button>
+      </Link>
+    </div>
   </section>)
+  }
 }
 export default DetailedArticle
